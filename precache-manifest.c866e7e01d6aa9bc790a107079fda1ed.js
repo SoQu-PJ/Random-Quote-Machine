807bb464e4d0dc2160786c89d87ea403b0e87108
SoQu-PJ <maniek999zano@gmail.com>
2023-02-09 20:12:27 +0100

    Updates

---
+++ precache-manifest.c866e7e01d6aa9bc790a107079fda1ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df544f37c1d5b137b63b",
+    "url": "/static/css/main.a22c91d7.chunk.css"
+  },
+  {
+    "revision": "df544f37c1d5b137b63b",
+    "url": "/static/js/main.df544f37.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "cc5e9e56c163788e6e16",
+    "url": "/static/js/2.cc5e9e56.chunk.js"
+  },
+  {
+    "revision": "1ed698c15cd075133fed",
+    "url": "/static/js/runtime~main.1ed698c1.js"
+  },
+  {
+    "revision": "c38db13bd5ad6522c5198a99e92bd1d6",
+    "url": "/index.html"
+  }
+];
